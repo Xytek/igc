7c1973dbec79100ba831e738d4d09073b834a1f6
Morten Spjøtvoll <36195919+Xytek@users.noreply.github.com>
2018-10-14 10:56:05 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,27 +11,31 @@ import (
 var lastUsed int
 var startTime = time.Now()
 
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
-}
+//func determineListenAddress() (string, error) {
+//	port := os.Getenv("PORT")
+//	if port == "" {
+//		return "", fmt.Errorf("$PORT not set")
+//	}
+//	return ":" + port, nil
+//}
 
 func main() {
 	db := TrackDB{}
 	db.Init()
 
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//addr, err := determineListenAddress()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+var port = process.env.PORT || 8000;
 
 	http.HandleFunc("/igcinfo/api", handlerAPI)
 	http.HandleFunc("/igcinfo/api/igc", handlerAPIIGC)
 	http.HandleFunc("/igcinfo/api/igc/", handlerAPIIGCMORE)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
-	}
+	server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
+	//if err := http.ListenAndServe(addr, nil); err != nil {
+	//	panic(err)
+	//}
 }
